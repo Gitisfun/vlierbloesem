716e2f00261bd747c16a00d93b4152c65d9e40d7
Gitisfun <christianvandenputtelaar@gmail.com>
2023-03-15 23:13:03 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-background"></div>
+</template>
+
+<style>
+* {
+  margin: 0;
+}
+.app-background {
+  min-height: 100vh;
+  background-image: url("~~/elderflower.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
